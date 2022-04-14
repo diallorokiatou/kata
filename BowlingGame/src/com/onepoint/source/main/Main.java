9532bfc8341aaa11ebfc3a70f6d5339f8b47db70
Rokiatou DIALLO <rokiatou.diallo@cgi.com>
2022-04-14 22:51:55 +0200

    add main

---
+++ BowlingGame/src/com/onepoint/source/main/Main.java
@@ -0,0 +1,12 @@
+package com.onepoint.source.main;
+
+import com.onepoint.source.model.Bowling;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Bowling bowling = new Bowling();
+		bowling.start();
+	}
+
+}
